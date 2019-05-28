fdeb3388ea9699467ab493710eca51f41e915092
Blair Lochrie <bllochri@microsoft.com>
2019-05-28 14:45:09 +0100

    Update averageoflist.py

--- averageoflist.py
+++ averageoflist.py
@@ -4,3 +4,5 @@ for num_stored in [2, 554, 66, 7854, 8] :
     list_count = list_count + 1
     list_total = list_total + num_stored
 print("average of List", list_total / list_count)
+print("Checking that GitHub Commits Work OK")
+
