0d9bb88712c249db85915fc6899fd785d1e98319
Blair Lochrie <bllochri@microsoft.com>
2019-05-29 13:24:00 +0100

    Update 5_2.py

--- PythonForEveryone/Assignment 5/5_2.py
+++ PythonForEveryone/Assignment 5/5_2.py
@@ -5,18 +5,18 @@ while True :
     if sval == "done" :
         break
     try:
-        fval = int(sval)
+        ival = int(sval)
     except:
         print("Invalid input")
         continue
     if smallest_num is None :
-        smallest_num = fval
-    elif fval < smallest_num :
-        smallest_num = fval
+        smallest_num = ival
+    elif ival < smallest_num :
+        smallest_num = ival
     if largest_num is None :
-        largest_num = fval
-    elif fval > largest_num :
-        largest_num = fval
+        largest_num = ival
+    elif ival > largest_num :
+        largest_num = ival
 
 print("Maximum is", largest_num)
 print("Minimum is", smallest_num)
